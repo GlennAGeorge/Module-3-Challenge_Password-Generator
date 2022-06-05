06a404dbf7a73b18c2a6afadc4a5ec903f287112
Glenn George <ggpersonaltraining@gmail.com>
2022-06-05 19:17:53 +1000

    Inital prompts and option variables

--- script.js
+++ script.js
@@ -19,3 +19,21 @@ lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r"
 // Uppercase Letters
 upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
+// Generate Password
+//Function (First prompt)
+function generatePassword() {
+    clickGeneratePassword = parseInt(prompt("Choose password length between 8 and 128"));
+ 
+    //If nothing is selected
+    if (!clickGeneratePassword) {
+        alert("Password length required");
+    } else if (clickGeneratePassword < 8 || clickGeneratePassword > 128) {
+
+    } else {
+        //Remaining prompts 
+        selectSymbol = confirm("Do you require special symbols?");
+        selectNumber = confirm("Do you require numbers?");
+        selectUppercase = confirm("Do you require Uppercase letters?");
+        selectLowercase = confirm("Do you requireLowercase letters?");
+    };
+}
